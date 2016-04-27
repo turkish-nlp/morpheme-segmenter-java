04c134a2e436b8eac3ddf0960122aed0677d191b
Ahmet Ustun <ahmet.ustun@cs.com.tr>
2016-04-27 16:00:08 +0300

    AllomorphCatcher added for testing

---
+++ src/main/java/core/AllomorphCatcher.java
@@ -0,0 +1,53 @@
+package core;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by ahmetu on 27.04.2016.
+ */
+public class AllomorphCatcher {
+
+    private String fileSegmentationInput;
+
+    private WordVectors vectors;
+
+    public AllomorphCatcher(String fileVectorInput, String fileSegmentationInput) {
+        try {
+            vectors = WordVectorSerializer.loadTxtVectors(new File(fileVectorInput));
+            this.fileSegmentationInput = fileSegmentationInput;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void TestAnalogy(){
+        String a = "kedi";
+        String b = "kediler";
+        String c = "köpek";
+        String d = "köpekler";
+
+        Collection<String> neighboors_a = vectors.wordsNearest(Arrays.asList(b, c), Arrays.asList(a), 10);
+        System.out.println("***************************");
+        for (String s : neighboors_a) {
+            System.out.println(s);
+        }
+
+        Collection<String> neighboors_b = vectors.wordsNearest(Arrays.asList(a, d), Arrays.asList(b), 10);
+        System.out.println("***************************");
+        for (String s : neighboors_b) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        AllomorphCatcher ac = new AllomorphCatcher(args[0], args[1]);
+        ac.TestAnalogy();
+    }
+
+}
