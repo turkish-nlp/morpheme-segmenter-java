f8e1ce53a2cb7f91004b7bd1a7c7232e48697b89
ahmet <ahmetustun@gmail.com>
2016-06-19 02:43:35 +0300

    segmentation

--- src/main/java/core/FrequencyProcessor.java
+++ src/main/java/core/FrequencyProcessor.java
@@ -1,5 +1,6 @@
 package core;
 
+import com.sun.tools.javac.util.*;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.output.*;
 import org.apache.commons.io.output.ByteArrayOutputStream;
@@ -7,6 +8,7 @@ import tries.TrieST;
 
 import java.io.*;
 import java.util.*;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -14,9 +16,40 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class FrequencyProcessor {
 
+
+    private class Morpheme {
+
+        public String name;
+        public List<TrieST> trieBelonged = new ArrayList<>();
+        public boolean isBoundary;
+
+        public Morpheme(String name, boolean isBoundary) {
+            this.name = name;
+            this.isBoundary = isBoundary;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Morpheme)) return false;
+
+            Morpheme morpheme = (Morpheme) o;
+
+            return name.equals(morpheme.name);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+    }
+
+
     private Map<String, TrieST> trieList = new HashMap<>();
     private Map<String, Integer> morphemeFreq = new ConcurrentHashMap<>();
     private Map<String, Set<String>> wordBoundary = new ConcurrentHashMap<>();
+    private Map<Morpheme, Integer> morphemeFreq_new = new ConcurrentHashMap<>();
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -24,6 +57,7 @@ public class FrequencyProcessor {
         FrequencyProcessor fp = new FrequencyProcessor();
         fp.generateTrieList(args[0]);
         fp.generateBoundaryListforBaseline(3);
+
 
         /*
         fp.trieList.keySet().parallelStream().forEach((n) -> {
@@ -33,7 +67,7 @@ public class FrequencyProcessor {
 
 
         for (String key : fp.trieList.keySet()) {
-            fp.calcuateFrequency(fp.trieList.get(key), fp.wordBoundary.get(key));
+            fp.determineSegmentation(fp.trieList.get(key), fp.wordBoundary.get(key));
         }
 
 
@@ -66,6 +100,74 @@ public class FrequencyProcessor {
                     morphemeFreq.put(morpheme, 1);
                 }
             }
+        }
+    }
+
+    private void determineSegmentation(TrieST st, Set<String> boundaries) {
+
+        Map<String, Integer> nodeList = st.getWordList();
+
+        for (String boundary : boundaries) {
+            nodeList.put(boundary + "$", 1);
+        }
+
+        for (String node : nodeList.keySet()) {
+            if (node.endsWith("$")) {
+
+                Stack<String> morphmeStack = new Stack<>();
+
+                String current = "";
+                boolean found = false;
+                for (String boundary : boundaries) {
+                    if (node.startsWith(boundary) && !node.equals(boundary + "$")) {
+                        current = boundary;
+                        found = true;
+                    }
+                }
+                String morpheme = node.substring(current.length(), node.length() - 1);
+                morphmeStack.add(morpheme);
+
+                String word = node.substring(0, current.length());
+                doSegmentation(word, boundaries, morphmeStack);
+
+                String segmentation = morphmeStack.pop();
+                int a = morphmeStack.size();
+                for (int i = 0; i < a; i++) {
+                    String popped = morphmeStack.pop();
+                    segmentation = segmentation + " + " + popped;
+                    Morpheme tmp = new Morpheme(popped, boundaries.contains(popped));
+                    if (!morphemeFreq_new.containsKey(tmp)) {
+                        tmp.trieBelonged.add(st);
+                        morphemeFreq_new.put(tmp, 1);
+                    } else {
+                        if (!tmp.trieBelonged.contains(st))
+                            tmp.trieBelonged.add(st);
+                        morphemeFreq_new.put(tmp, morphemeFreq_new.get(tmp) + 1);
+                    }
+                }
+                System.out.println(segmentation);
+            }
+        }
+
+    }
+
+    private void doSegmentation(String node, Set<String> boundaries, Stack<String> morphmeStack) {
+
+        if (!node.equals("")) {
+            String current = "";
+            boolean found = false;
+            for (String boundary : boundaries) {
+                if (node.startsWith(boundary) && !node.equals(boundary)) {
+                    current = boundary;
+                    found = true;
+                }
+            }
+            String morpheme = node.substring(current.length(), node.length());
+            morphmeStack.add(morpheme);
+
+            String word = node.substring(0, current.length());
+
+            doSegmentation(word, boundaries, morphmeStack);
         }
     }
 
