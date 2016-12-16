5bd680c4a53fdaf947d082ddea1162d953e4393c
Murathan <murathankurfali@gmail.com>
2016-12-16 12:06:57 +0300

    morfessor

--- src/main/java/core/comparison/PrepareMorfessorData.java
+++ src/main/java/core/comparison/PrepareMorfessorData.java
@@ -1,6 +1,7 @@
 package core.comparison;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
@@ -10,14 +11,18 @@ import java.util.StringTokenizer;
  */
 public class PrepareMorfessorData {
 
+    private static Charset ascii = Charset.forName("ASCII");
+
+
     public static void main(String[] args) throws IOException {
         HashMap<String, Integer> wordlist = new HashMap<>();
-        String trieFile = "wordlist_german_recursive.txt";
-        String mcFile = "mc_wordlist-2010_ger.txt";
+        String trieFile = "wordlist_turkish_recursive.txt";
+        String mcFile = "mc_wordlist-2010_tur.txt";
 
         BufferedReader reader = new BufferedReader(new FileReader(new File(mcFile)));
         String line = null;
         while ((line = reader.readLine()) != null) {
+
             StringTokenizer st = new StringTokenizer(line, " ");
 
             int freq = Integer.parseInt(st.nextToken());
@@ -32,15 +37,18 @@ public class PrepareMorfessorData {
 
         reader = new BufferedReader(new FileReader(new File(trieFile)));
         while ((line = reader.readLine()) != null) {
-
+            line = line.replaceAll("ö", "O").replaceAll("ç", "C").replaceAll("ü", "U").replaceAll("ı", "I").replaceAll("ğ", "G").replaceAll("ü", "U").replaceAll("ş", "S");
             if (wordlist.containsKey(line)) {
-                train.put(line, wordlist.get(line));
+                if(trieFile.contains("tur"))
+                    train.put(line.replaceAll("ö", "O").replaceAll("ç", "C").replaceAll("ü", "U").replaceAll("ı", "I").replaceAll("ğ", "G").replaceAll("ü", "U").replaceAll("ş", "S"), wordlist.get(line));
+                else
+                    train.put(line, wordlist.get(line));
             } else {
                 train.put(line, 1);
             }
         }
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File(trieFile + "_prepared")));
+        PrintWriter writer = new PrintWriter(new File(trieFile + "_prepared"), "ASCII");
 
         for (String word : train.keySet()) {
             writer.write(train.get(word)+ " " + word + "\n");
